00b1542487de50c136da633b52ae93b57ab61ba3
Nicolas Raidelet <nicolas.raidelet@irstea.fr>
2015-07-17 19:24:28 +0200

    Correction de la route de départ.

--- app/js/app.js
+++ app/js/app.js
@@ -4,8 +4,7 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers', 'myApp.runs']).
         config(['$routeProvider', function ($routeProvider) {
-                $routeProvider.when('/personnage', {templateUrl: 'partials/personnage.html', controller: 'CtrPersonnage'});
                 $routeProvider.when('/talents', {templateUrl: 'partials/talents.html', controller: 'CtrTalents'});
                 $routeProvider.when('/competences', {templateUrl: 'partials/competences.html', controller: 'CtrCompetences'});
-                $routeProvider.otherwise({redirectTo: '/personnage'});
+                $routeProvider.otherwise({redirectTo: '/talents'});
             }]);
